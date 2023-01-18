03f34a3a0d31cfd106a085e5d0e96f15f0ab684e
Arthur Moreira <arthurmcs96@gmail.com>
2023-01-18 17:15:34 -0300

    create a new seeds and isolate app files from back end

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify';
+import { prisma } from './lib/prisma';
+
+export async function appRoutes(app: FastifyInstance) {
+
+    app.get('/', async () => {
+        const habits = await prisma.habit.findMany({
+            where: {
+                title: {
+                    startsWith: 'Treinar'
+                }
+            }
+        })
+
+        return habits
+    })
+
+}
+
