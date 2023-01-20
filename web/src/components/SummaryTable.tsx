f5db96001a0350719908d36e7f6cf4dd80cd5eb6
Arthur Moreira <arthurmcs96@gmail.com>
2023-01-20 12:42:44 -0300

    create the main screen

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,37 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginng"
+import { HabitDay } from "./HabitDay"
+
+const weekdays = ['D', 'S', 'T', 'Q', 'Q', 'S', 's']
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+
+export default function SummaryTable() {
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                {
+                    weekdays.map((weekday, i) => (
+                        <div key={`${weekday} - ${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+                            {weekday}
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {
+                    summaryDates.map((day) => <HabitDay key={day.toString()} />)
+                }
+                {
+                    amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                        return <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+                    })
+                }
+            </div>
+        </div>
+    )
+}
